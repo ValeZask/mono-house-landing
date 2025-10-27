0317284b47d85dec55f8a8e46a8ceb8bfe1c46fb
Vale <netbakovchyngyz@gmail.com>
2025-10-27 09:35:41 +0600

    Изменил описание в херо секции и в о нас

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ export function Hero() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.2 }}
         >
-          Дизайн и мебелирование интерьеров в Бишкеке
+          Дизайн мебели и мебелировка помещений в Бишкеке
         </motion.p>
       </div>
     </section>
